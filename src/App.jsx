04305e8e2510df349d598a8a6cad44019666432b
Saim Shahzad <saimshehzad2030@gmail.com>
2023-06-11 00:06:58 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import Navbar from './components/navbar'
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <div id="cv">
+      <div className="rows">
+      <h3>
+        Name: 
+      </h3>
+      <h3>
+        Mirza Saim Shahzad
+      </h3>
+      </div>
+
+      <div className="rows">
+      <h3>
+        F Name: 
+      </h3>
+      <h3>
+        Shahzad Murtaza
+      </h3>
+      </div>
+
+      <div className="rows">
+      <h3>
+        Age:  
+      </h3>
+      <h3>
+        21
+      </h3>
+      </div>
+    </div>
+   
+     </> 
+      )
+}
+
+export default App
---
+++ src/components/Button1.jsx
@@ -0,0 +1,12 @@
+
+function button1() {
+  return (
+   <>
+ <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+   </>
+  )
+}
+
+export default button1
